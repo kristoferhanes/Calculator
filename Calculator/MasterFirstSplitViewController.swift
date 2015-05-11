a5a31dff2129dae83306c722c8f81044fee18157
Kristofer Hanes <kristoferhanes@222153.local>
2015-05-11 16:10:39 -0700

    Added master first setting for splitviewcontroller

---
+++ Calculator/MasterFirstSplitViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MasterFirstSplitViewController.swift
+//  Calculator
+//
+//  Created by Kristofer Hanes on 2015 05 11.
+//  Copyright (c) 2015 Kristofer Hanes. All rights reserved.
+//
+
+import UIKit
+
+class MasterFirstSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.delegate = self
+  }
+
+  func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool {
+    return true
+  }
+
+}
